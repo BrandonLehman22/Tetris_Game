5fbf93c357efc6a1289d3dc058ac14f83d10a819
Brandon Lehman <brando822@outlook.com>
2023-12-29 13:54:16 -0800

    first commit

---
+++ src/main/GamePanel.java
@@ -0,0 +1,75 @@
+package main;
+
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyListener;
+
+
+
+public class GamePanel extends JPanel implements Runnable { 
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+        
+    public int FPS = 60;
+    Thread gameThread;
+    PlayManager pm;
+
+    public static Sound music = new Sound();
+    public static Sound se = new Sound();
+
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension (WIDTH, HEIGHT));
+        this.setBackground(Color.LIGHT_GRAY);
+        this.setLayout(null);
+
+        this.addKeyListener(new KeyHandler());
+        this.setFocusable(true);
+
+        pm = new PlayManager();
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+        
+        music.play(0, true);
+        music.loop();
+    }
+    public void run() {
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+    private void update() {
+        if (KeyHandler.pausePressed ==  false && pm.gameOver == false) {
+            pm.update();
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+        pm.draw(g2);
+    }
+
+
+}
